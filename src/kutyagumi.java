5b4d1c86ed4e04764c72ab8b48b3dd59090746b8
peteri gabor <peteri@t-online.hu>
2020-10-13 15:41:22 +0200

    minimum keresessel bovitve

--- src/kutyagumi.java
+++ src/kutyagumi.java
@@ -3,7 +3,18 @@ public class kutyagumi {
     public static void main(String[] args) {
         int tomb[] =new int[100];
         for (int i = 0; i <tomb.length ; i++) {
-            tomb[i]=(int)(Math.random()*100+1);
+            tomb[i]=(int)((Math.random()*100)+1);
         }
+        System.out.println("a legkisebb ertek: "+minimum(tomb));
+    }
+
+    public static int minimum(int[] tomb){
+        int min=Integer.MAX_VALUE;
+        for (int i = 0; i < tomb.length; i++) {
+            if(tomb[i]<min){
+             min=tomb[i];
+            }
+        }
+        return min;
     }
 }
