22d1836883e3cba79648eed42b2eccfb6689845f
Zoltan Marai <zoltanmarai51@gmail.com>
2020-10-13 16:55:57 +0200

    count of odds number

--- src/kutyagumi.java
+++ src/kutyagumi.java
@@ -6,6 +6,8 @@ public class kutyagumi {
             tomb[i]=(int)((Math.random()*100)+1);
         }
         System.out.println("a legkisebb ertek: "+minimum(tomb));
+        System.out.println(" A tömb legnagyobb eleme: " + maxOfArray(tomb));
+        System.out.println(" A páratlan elemek száma: " + oddsNumbersInArray(tomb));
     }
 
     public static int minimum(int[] tomb){
@@ -16,5 +18,28 @@ public class kutyagumi {
             }
         }
         return min;
+    }
+    public static int maxOfArray ( int [] nums) {
+        int max= 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if ( max < nums [i]) {
+                max = nums [i];
+            }
+
+        }
+        return max;
+    }
+    public static int oddsNumbersInArray(int[] nums) {
+
+        int counter = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] % 2 != 0) {
+                counter++;
+            }
+        }
+        return counter;
     }
 }
