1c0bb9d0359424af8bba89385b9a9b3e5b4351a7
peteri gabor <peteri@t-online.hu>
2020-10-13 15:19:21 +0200

    ezaz

---
+++ src/kutyagumi.java
@@ -0,0 +1,9 @@
+public class kutyagumi {
+
+    public static void main(String[] args) {
+        int tomb[] =new int[100];
+        for (int i = 0; i <tomb.length ; i++) {
+            tomb[i]=(int)(Math.random()*100+1);
+        }
+    }
+}
